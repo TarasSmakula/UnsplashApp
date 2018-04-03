348d7c76e310093e1b1cf22b712a6968e93d2667
Taras <tarassmakula@gmail.com>
2018-04-03 20:48:34 +0300

    Create single photo activity

--- app/src/main/java/com/gentledevs/unsplashapp/photo/SinglePhotoActivity.kt
+++ app/src/main/java/com/gentledevs/unsplashapp/photo/SinglePhotoActivity.kt
@@ -1,12 +1,14 @@
-package com.gentledevs.unsplashapp
+package com.gentledevs.unsplashapp.photo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gentledevs.unsplashapp.R
 
-class MainActivity : AppCompatActivity() {
+class SinglePhotoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_single_photo)
+
     }
 }
